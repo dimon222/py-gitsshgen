7708197cc6550c3d1f946409a298a701b3cad428
Dmitry Romanenko <Dmitry@Romanenko.in>
2020-01-26 19:35:40 -0500

    Release 1.2.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="gitsshgen",
-    version="1.2.1",
+    version="1.2.2",
     author="Dmitry Romanenko",
     author_email="Dmitry@Romanenko.in",
     description="Automatic generation of SSH keys for VCS",
